b0a882cfce29b4471b9a49aa2fa940a63006e966
md janbaz <twilio.send.sms10@gmail.com>
2024-06-06 23:09:16 +0530

    this is my commit

--- src/main/java/com/nobrokers/nobrokers/controller/LoginController.java
+++ src/main/java/com/nobrokers/nobrokers/controller/LoginController.java
@@ -12,7 +12,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class LoginController {
+// this is comment
 
+    //thisnis good
     @Autowired
     private EmailVerificationService emailVerificationService;
 
